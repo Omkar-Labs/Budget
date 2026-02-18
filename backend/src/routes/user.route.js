268da3044fabd08b5375c11753ed4d23332d0cdf
Omkar-Labs <omkartripathi36@gmail.com>
2026-02-18 22:34:46 +0530

    transaction route build

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { loginUser, logOutUser, refreshAccessToken, registerUser,generateOtp,verifyOtp,getUserProfile } from "../controllers/user.controller.js";
+import { addTransaction, deleteTransaction, updateTransaction,transactionStats ,singleTransaction} from "../controllers/transaction.controller.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
 
@@ -19,8 +20,16 @@ router.route("/logout").post(verifyToken , logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 
 router.route("/forgot-password").post(generateOtp)
+
 router.route("/verify-otp").patch(verifyOtp)
 
 router.route("/profile").get(verifyToken, getUserProfile)
+
+router.route("/transactions").post(verifyToken, addTransaction)
+router.route("/transactions/:id").patch(verifyToken, updateTransaction)
+router.route("/transactions/:id").delete(verifyToken, deleteTransaction)
+
+router.route("/transactions/stats").get(verifyToken, transactionStats)
+router.route("/transactions/:id").get(verifyToken, singleTransaction)
 
 export default router
