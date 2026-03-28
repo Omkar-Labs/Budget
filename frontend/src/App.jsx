c55f96e214811d5a4a12ab87c4981811d9541a34
Omkar-Labs <omkartripathi36@gmail.com>
2026-03-28 15:41:33 +0530

    Improve loading State

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import React, { useState } from 'react'
 import { useLoaderData, Outlet } from 'react-router-dom';
 import Header from './components/dashboard/Header';
 import Profile from './components/dashboard/Profile';
+import { useNavigation } from 'react-router-dom';
+import Welcome from './assets/Welcome.json';
+import { Player } from '@lottiefiles/react-lottie-player'
 
 
 
@@ -12,14 +15,24 @@ function App() {
     const initialData = useLoaderData();
     const [data, setData] = useState(initialData);
     const [profileOpen, setProfileOpen] = useState(false);
+    const navigate = useNavigation();
+    const isLoading = navigate.state === "loading";
 
     return (
         <>
             <User.Provider value={{ data, setData }}>
-                <div className='h-screen w-full flex flex-col bg-linear-to-br from-[#1a0b2e] via-[#2d1b4d] to-[#4a1d4d]'>
+                 <div className='h-screen w-full flex flex-col bg-linear-to-br from-[#1a0b2e] via-[#2d1b4d] to-[#4a1d4d]'>
+                    {isLoading ? <Player
+                        autoplay
+                        loop
+                        src={Welcome}
+                        style={{ height: '300px', width: '300px' }}
+                    >
+                    </Player> : <>
                     <Header setProfileOpen={setProfileOpen} profileOpen={profileOpen} />
                     <Outlet />
                    {profileOpen && <Profile profileOpen={profileOpen} setProfileOpen={setProfileOpen} />}
+                </>}
                 </div>
 
             </User.Provider>
