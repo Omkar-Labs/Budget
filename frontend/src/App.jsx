29e9da8a8975eb4c768949a30d019ed20b552c1c
Omkar-Labs <omkartripathi36@gmail.com>
2026-03-28 15:51:16 +0530

    Animation bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,22 +20,20 @@ function App() {
 
     return (
         <>
-            <User.Provider value={{ data, setData }}>
-                 <div className='h-screen w-full flex flex-col bg-linear-to-br from-[#1a0b2e] via-[#2d1b4d] to-[#4a1d4d]'>
-                    {isLoading ? <Player
+        {isLoading ? <div className='h-screen w-full flex flex-col bg-linear-to-br from-[#1a0b2e] via-[#2d1b4d] to-[#4a1d4d]'> <Player
                         autoplay
                         loop
                         src={Welcome}
                         style={{ height: '300px', width: '300px' }}
                     >
-                    </Player> : <>
+                    </Player> </div> : 
+            <User.Provider value={{ data, setData }}>
+                <div className='h-screen w-full flex flex-col bg-linear-to-br from-[#1a0b2e] via-[#2d1b4d] to-[#4a1d4d]'>
                     <Header setProfileOpen={setProfileOpen} profileOpen={profileOpen} />
                     <Outlet />
                    {profileOpen && <Profile profileOpen={profileOpen} setProfileOpen={setProfileOpen} />}
-                </>}
                 </div>
-
-            </User.Provider>
+            </User.Provider>}
         </>
     )
 }
