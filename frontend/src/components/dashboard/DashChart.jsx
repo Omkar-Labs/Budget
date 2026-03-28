ca260e743b05b2ab96ebc04649d76f8cd1bc93f8
Omkar-Labs <omkartripathi36@gmail.com>
2026-03-28 20:39:21 +0530

    Css bug

--- frontend/src/components/dashboard/DashChart.jsx
+++ frontend/src/components/dashboard/DashChart.jsx
@@ -26,8 +26,26 @@ const DashChart = () => {
         setHoverData({ x: null, y: null });
       }
     },
+    Touch: {
+      enabled: true,
+      mode: 'nearest',
+      external: (context) => {
+        const { chart, tooltip } = context;
+        if (!tooltip._active || tooltip._active.length === 0) {
+          setHoverData({ x: null, y: null });
+          return;
+        }
+        const activePoint = tooltip._active[0].element;
+        const index = activePoint.$context.dataIndex;
+        const xValue = category[index];
+        const yValue = data.stats.categoryStats[index].totalAmount;
+        setHoverData({ x: xValue, y: yValue });
+      }
+    },
     plugins: {
-      tooltip: { enabled: false }
+      tooltip: { enabled: false,
+        events: ['click','touchstart','touchmove','touchend','touchcancel','pointerenter','pointerleave']
+       }
     },
 
   }
@@ -72,7 +90,7 @@ const DashChart = () => {
   }, [])
   return (
     <div className="dashboard w-full h-auto min-h-[340px] lg:h-80 row-span-1 lg:row-span-3 relative p-4 lg:p-6 flex flex-col items-center justify-center bg-white/10 backdrop-blur-xl rounded-lg border-white/20 shadow-[0_0_30px_rgba(255,0,255,0.1)]">
-      <div className={`transition-opacity duration-300 ${hoverData.x ? 'opacity-100' : 'opacity-0'} z-10 hidden sm:block absolute top-5 right-5 pointer-events-none`}>
+      <div className={`transition-opacity duration-300 ${hoverData.x ? 'opacity-100' : 'opacity-0'} z-10  sm:block absolute top-5 right-5 pointer-events-none `}>
         <div className="p-4 rounded xl bg-white/10 border border-white/20 backdrop-blur-md shadow-2xl">
           <p className="text-pink-400 text-xs uppercase font-bold tracking-widest">{hoverData.x}</p>
           <h3 className="text-2xl font-bold text-white">{symbol}{hoverData.y?.toLocaleString()}</h3>
